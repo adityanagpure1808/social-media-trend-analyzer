8619ef8b2b6393a05e556bb17c1b534e6de3ba85
adityanagpure1808 <ngpaditya503@gmail.com>
2026-02-11 15:06:41 +0530

    prepare backend for render deployment

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,19 @@
-import sqlite3
-import os
+# import sqlite3
+# import os
 
-DB_PATH = os.getenv("DB_PATH", "database.db")
+# DB_PATH = os.getenv("DB_PATH", "database.db")
+
+# def get_db():
+#     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+#     conn.row_factory = sqlite3.Row
+#     return conn
+import os
+import sqlite3
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "database.db")
+
+os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
 def get_db():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
